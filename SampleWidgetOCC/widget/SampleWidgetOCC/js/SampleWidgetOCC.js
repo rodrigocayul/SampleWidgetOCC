3f749e14797cb019318a69465aac75a882c6cc8a
Rodrigo Cayul <rodrigo.cayul@gmail.com>
2019-06-12 10:55:11 -0400

    Add files via upload

---
+++ SampleWidgetOCC/widget/SampleWidgetOCC/js/SampleWidgetOCC.js
@@ -0,0 +1,37 @@
+
+define(
+    //-------------------------------------------------------------------
+    // DEPENDENCIES
+    //-------------------------------------------------------------------
+    ['jquery', 'knockout','ccRestClient','navigation','ccConstants'],
+    //-------------------------------------------------------------------
+    // MODULE DEFINITION
+    //-------------------------------------------------------------------
+    function ($, ko,ccRestClient,navigation,ccConstants) {
+
+        "use strict";
+
+        var widgetViewModel;
+
+        return {
+			
+			titulo: ko.observable(),			
+			
+			onLoad: function (widget){
+				widgetViewModel = widget;
+				
+				widgetViewModel.titulo("1° Titulo");
+				
+				setTimeout(function(){					
+					widgetViewModel.titulo("2° Nuevo Titulo");				
+				},3000);
+				
+	
+			},
+            beforeAppear: function()
+			{	   
+    
+            }
+        };
+    }
+);
